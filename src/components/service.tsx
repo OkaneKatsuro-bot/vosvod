e3b5057b8de6276acc35ea319d666316072358c6
Franz <f.o.larionov@gmail.com>
2025-05-05 17:05:27 +0300

    fix

--- src/components/service.tsx
+++ src/components/service.tsx
@@ -8,7 +8,7 @@ export function ThreeDCardDemo() {
     {
       title: "Nature Explorer",
       description: "Discover the beauty of untouched wilderness and rare species.",
-      imageUrl: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      imageUrl: "img_3742.jpg"
     },
     {
       title: "City Lights",
