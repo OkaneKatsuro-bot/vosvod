1e8194fb488e003056881c077c0ce651048f39f0
Franz <f.o.larionov@gmail.com>
2025-05-15 11:31:23 +0300

    fix

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,10 @@
+import YandexMap from '@/components/YandexMap';
+
+export default function ContactsPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 32, marginBottom: 20 }}>Контакты</h1>
+      <YandexMap />
+    </div>
+  );
+}
