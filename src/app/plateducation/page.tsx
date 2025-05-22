b6bf79f4473591907af70ac94e8d2cb756dfc737
Franz <f.o.larionov@gmail.com>
2025-05-22 15:08:32 +0300

    fix

--- src/app/plateducation/page.tsx
+++ src/app/plateducation/page.tsx
@@ -22,12 +22,12 @@ const PaidServicesPage = () => {
               </p>
               <ul className="list-disc list-inside space-y-2">
                 <li>
-                  <Link href="https://vosvod.spb.ru/wp-content/uploads/2023/02/Положение-о-порядке-предоставления-платных-образователных-услуг.pdf" target="_blank" className="text-blue-700 hover:underline">
+                  <Link href="/education/Положение-о-порядке-предоставления-платных-образователных-услуг-3.pdf" target="_blank" className="text-blue-700 hover:underline">
                     Положение о порядке оказания платных образовательных услуг
                   </Link>
                 </li>
                 <li>
-                  <Link href="https://vosvod.spb.ru/wp-content/uploads/2023/02/%D0%9F%D1%80%D0%B8%D0%BA%D0%B0%D0%B7-%D0%BE%D0%B1-%D1%83%D1%82%D0%B2%D0%B5%D1%80%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B8-%D1%81%D1%82%D0%BE%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D0%B8-%D0%BE%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D1%8F-%D0%B8-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BA-%D0%BD%D0%B5%D0%BC%D1%83.pdf" target="_blank" className="text-blue-700 hover:underline">
+                  <Link href="/education/Приказ-об-утверждении-стоимости-обучения-и-приложение-к-нему-3.pdf">
                     Образец договора об оказании платных образовательных услуг
                   </Link>
                 </li>
